4092714102e7ad09c412f9059dec918f367737a6
文堃 杜 <ken.bug@qq.com>
2019-04-26 17:00:46 +0800

    1. 新增部分对iOS12和XR、MAX的屏幕判断 2. 新增安全使用Block的宏定义

--- SCBasicComponents/Classes/Maceos/SCDefaultsUI.h
+++ SCBasicComponents/Classes/Maceos/SCDefaultsUI.h
@@ -18,15 +18,16 @@
 #define IsIOS9 ([[[UIDevice currentDevice] systemVersion] doubleValue] >= 9.0? YES : NO)
 #define IsIOS10 ([[[UIDevice currentDevice] systemVersion] doubleValue] >= 10.0? YES : NO)
 #define IsIOS11 ([[[UIDevice currentDevice] systemVersion] doubleValue] >= 11.0? YES : NO)
+#define IsIOS12 ([[[UIDevice currentDevice] systemVersion] doubleValue] >= 12.0? YES : NO)
 
 //屏幕宽高
 #define ScreenWidth ([UIScreen mainScreen].bounds.size.width)
 #define ScreenHeight ([UIScreen mainScreen].bounds.size.height)
 
-#define TopSafeHeight (IsScreen_5_8 ? 44 : 20)
-#define BottomSafeHeight (IsScreen_5_8 ? 34 : 0)
-#define NavigationHeight (IsScreen_5_8 ? 88 : 64)
-#define TabBarHeight (IsScreen_5_8 ? 83 : 49)
+#define TopSafeHeight (ScreenHeight >= IsScreen_5_8 ? 44 : 20)
+#define BottomSafeHeight (ScreenHeight >= IsScreen_5_8 ? 34 : 0)
+#define NavigationHeight (ScreenHeight >= IsScreen_5_8 ? 88 : 64)
+#define TabBarHeight (ScreenHeight >= IsScreen_5_8 ? 83 : 49)
 
 //屏幕尺寸
 #define IsScreen_3_5 (ScreenHeight == 480)
@@ -34,7 +35,7 @@
 #define IsScreen_4_7 (ScreenHeight == 667)
 #define IsScreen_5_5 (ScreenHeight == 736)
 #define IsScreen_5_8 (ScreenHeight == 812)
-#define IsScreen_6_1 (ScreenHeight == 896) //6.1寸的XR和6.5寸的MAX相同
+#define IsScreen_6_1_6_5 (ScreenHeight == 896) //6.1寸的XR和6.5寸的MAX相同
 
 //颜色和透明度设置
 #define RGBA(r,g,b,a) [UIColor colorWithRed:(float)r/255.0f green:(float)g/255.0f blue:(float)b/255.0f alpha:a]
@@ -49,6 +50,9 @@
 #define StringToDouble(a)  [[NSNumber numberWithString:a] doubleValue]
 #define StringToFloat(a)   [[NSNumber numberWithString:a] floatValue]
 #define StringToInteger(a) [[NSNumber numberWithString:a] integerValue]
+
+//安全Blcok使用
+#define SCSafeBlock(BlockName, ...) ({ !BlockName ? nil : BlockName(__VA_ARGS__); })
 
 #endif
 
