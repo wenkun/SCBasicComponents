e1246f6912fd785baa56d25bd222f98e430e66d0
文堃 杜 <ken.bug@qq.com>
2019-09-26 17:30:08 +0800

    优化SCLog

--- SCBasicComponents/Classes/Tools/SCLog/SCLogManager.h
+++ SCBasicComponents/Classes/Tools/SCLog/SCLogManager.h
@@ -25,19 +25,25 @@ typedef enum : NSUInteger {
     SCLogLevelPrivate,
 } SCLogLevel;
 
+
+#if DEBUG
+
+//生产log
+#define SCLog(FORMAT, ...) if([SCLogManager share].level >= SCLogLevelProduct) [SCLogManager logWithFormat:(FORMAT @"\n %s[%d]"), ##__VA_ARGS__, __FUNCTION__, __LINE__]
+//debug log
+#define SCDebugLog(FORMAT, ...) [SCLogManager logWithFormat:(@"[D]" FORMAT @"\n %s[%d]"), ##__VA_ARGS__, __FUNCTION__, __LINE__]
+//私有log
+#define SCPLog(FORMAT, ...) [SCLogManager logWithFormat:(@"[DP]" FORMAT @"\n %s[%d]"), ##__VA_ARGS__, __FUNCTION__, __LINE__]
+
+#else
+
 //生产log
 #define SCLog(FORMAT, ...) if([SCLogManager share].level >= SCLogLevelProduct) [SCLogManager logWithFormat:(FORMAT @"\n %s"), ##__VA_ARGS__, __FUNCTION__]
 //debug log
-#if DEBUG
-#define SCDebugLog(FORMAT, ...) [SCLogManager logWithFormat:(@"[D]" FORMAT @"\n %s[%d]"), ##__VA_ARGS__, __FUNCTION__, __LINE__]
-#else
 #define SCDebugLog(FORMAT, ...) if([SCLogManager share].level >= SCLogLevelDebug) [SCLogManager logWithFormat:(@"[D]" FORMAT @"\n %s[%d]"), ##__VA_ARGS__, __FUNCTION__, __LINE__]
-#endif
 //私有log
-#if DEBUG
-#define SCPLog(FORMAT, ...) [SCLogManager logWithFormat:(@"[DP]" FORMAT @"\n %s[%d]"), ##__VA_ARGS__, __FUNCTION__, __LINE__]
-#else
 #define SCPLog(FORMAT, ...) if([SCLogManager share].level == SCLogLevelPrivate) [SCLogManager logWithFormat:(@"[DP]" FORMAT @"\n %s[%d]"), ##__VA_ARGS__, __FUNCTION__, __LINE__]
+
 #endif
 
 @protocol SCLogManagerDelegate;
